cd2fbf8c8ed6920240c5c91dddbd911191da8b5d
Volkert de Buisonjé <volkertb@users.noreply.github.com>
2023-11-05 15:53:24 +0100

    Add `oss_hdaudio` directory, unpacked from http://www.4front-tech.com/developer/sources/stable/bsd/oss-v4.2-build2019-src-bsd.tar.bz2

---
+++ oss_hdaudio/hdaudio_mixers.h
@@ -0,0 +1,19 @@
+/*
+ * Purpose: Declarations of some functions and structures for HD audio mixers
+ */
+/*
+ * This file is part of Open Sound System
+ *
+ * Copyright (C) 4Front Technologies 1996-2008.
+ *
+ * This software is released under the BSD license.
+ * See the COPYING file included in the main directory of this source
+ * distribution for the license terms and conditions
+ */
+
+/*
+ * Prototype definitions for dedicated HDAudio codec/mixer drivers.
+ */
+
+extern int hdaudio_generic_mixer_init (int dev, hdaudio_mixer_t * mixer,
+				       int cad, int group);
